ea38a7097abcd75a499f2c111953ce0a68950868
pavinduLakshan <174084F@uom.lk>
2020-07-01 13:34:50 +0530

    remove unused code

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const UserRouter = require('./UserRouter')
-const NoteRouter = require('./NoteRouter');
 
 router.use('/api/users', UserRouter);
-router.use('/api/notes', NoteRouter);
 
 module.exports = router;
